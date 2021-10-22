87d5e32723e5fe59c250805e1bdf7dbb6e2b74bd
zhaixiuchuan <zhaixiuchuan@hotmail.com>
2021-10-22 17:59:40 +0800

    add single file

---
+++ c++/quote/single.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+    char t1[] = "a";
+    char t2[] = "b";
+    
+    int i = 10;
+    char t3[] = "c";
+    char t4[] = "hello";
+
+    float f = 12.34;
+
+    return 0;
+}
