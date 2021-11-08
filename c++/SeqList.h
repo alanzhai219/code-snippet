a7d949bba21c80dfa6be9f94aedf76ee5390ad3e
Alan Zhai <zhaixiuchuan@hotmail.com>
2021-11-08 08:34:09 +0800

    init list

---
+++ c++/SeqList.h
@@ -0,0 +1,27 @@
+#ifndef _SEQLIST_H_
+#define _SEQLIST_H_
+
+#include "List.h"
+
+namespace DS {
+
+template <typename T>
+class SeqList : public List<T> {
+    protected:
+        T* m_array;
+        int m_length;
+    
+    public:
+        bool insert(int idx, const T& elem);
+        bool remove(int idx);
+        bool set(int idx, const T& elem);
+        bool get(int idx, T& elem) const;
+        int length() const;
+        bool clear();
+        T& operator[](int idx);
+        virtual int capacity() const = 0;
+};
+
+}   // DS
+
+#endif // _SEQLIST_H_
